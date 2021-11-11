e28de41fd74c82c54d59b6aa20be856e3cb9a9d0
Darian Heede <darian.heede@getmercury.io>
2021-11-11 10:44:56 +0100

    Implemented first stage of connector: - Implemented first draft of spec - implemented check method using current endpoint - implemented discover catalog for endpoint forecast - implemented read method for full_refresh forecast endpoint - implemented Transform class to restructure forecast response.

---
+++ airbyte-integrations/connectors/source-weatherstack/source_weatherstack/source.py
@@ -0,0 +1,172 @@
+#
+# Copyright (c) 2021 Airbyte, Inc., all rights reserved.
+#
+
+
+from abc import ABC
+from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
+
+import requests
+import json
+from airbyte_cdk.sources import AbstractSource
+from airbyte_cdk.sources.streams import Stream
+from airbyte_cdk.sources.streams.http import HttpStream
+from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
+
+from .names import URL_BASE
+from .helpers import Transform 
+
+"""
+TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
+
+This file provides a stubbed example of how to use the Airbyte CDK to develop both a source connector which supports full refresh or and an
+incremental syncs from an HTTP API.
+
+The various TODOs are both implementation hints and steps - fulfilling all the TODOs should be sufficient to implement one basic and one incremental
+stream from a source. This pattern is the same one used by Airbyte internally to implement connectors.
+
+The approach here is not authoritative, and devs are free to use their own judgement.
+
+There are additional required TODOs in the files within the integration_tests folder and the spec.json file.
+"""
+
+
+# Basic full refresh stream
+class WeatherstackStream(HttpStream, ABC):
+
+    url_base = URL_BASE
+    
+    def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
+        return None
+
+    def request_params(
+        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> MutableMapping[str, Any]:
+        return {}
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        yield {response.json()}
+
+
+class Forecast(WeatherstackStream):
+
+    primary_key = None
+
+    def __init__(self, config: Mapping[str, Any], **kwargs):
+        super().__init__(**kwargs)
+        self.config = config
+
+    def path(self, **kwargs) -> str:
+        path = f"forecast"
+        return path
+
+    def request_params(
+        self,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, any] = None,
+        next_page_token: Mapping[str, Any] = None
+    ) -> MutableMapping[str, Any]:
+        return {
+            "access_key": self.config.get("access_key"),
+            "query": self.config.get("query"),
+            "forecast_days": self.config.get("forecast_days"),
+            "hourly": 1 if self.config.get("hourly") else 0 
+        }
+
+    def parse_response(
+        self,
+        response: requests.Response,
+        stream_state: Mapping[str, Any],
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> Iterable[Mapping]:
+        response = response.json()
+        restructured_response = Transform.restructure_response(response, "forecast")
+        return [restructured_response]
+
+# Basic incremental stream
+class IncrementalWeatherstackStream(WeatherstackStream, ABC):
+    """
+    TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
+         if you do not need to implement incremental sync for any streams, remove this class.
+    """
+
+    # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
+    state_checkpoint_interval = None
+
+    @property
+    def cursor_field(self) -> str:
+        """
+        TODO
+        Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
+        usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
+
+        :return str: The name of the cursor field.
+        """
+        return []
+
+    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
+        """
+        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
+        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
+        """
+        return {}
+
+
+    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
+        """
+        TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
+
+        Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
+        This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
+        section of the docs for more information.
+
+        The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
+        necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
+        This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
+
+        An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
+        craft that specific request.
+
+        For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
+        this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
+        till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
+        the date query param.
+        """
+        raise NotImplementedError("Implement stream slices or delete this method!")
+
+
+# Source
+class SourceWeatherstack(AbstractSource):
+    def check_connection(self, logger, config) -> Tuple[bool, any]:
+        """
+        TODO: Implement a connection check to validate that the user-provided config can be used to connect to the underlying API
+
+        See https://github.com/airbytehq/airbyte/blob/master/airbyte-integrations/connectors/source-stripe/source_stripe/source.py#L232
+        for an example.
+
+        :param config:  the user-input config object conforming to the connector's spec.json
+        :param logger:  logger object
+        :return Tuple[bool, any]: (True, None) if the input config can be used to connect to the API successfully, (False, error) otherwise.
+        """
+
+        url_base = URL_BASE
+        endpoint = "current"
+        url = url_base + endpoint
+        access_key = config.get("access_key")
+        query = config.get("query")
+        payload = {'access_key': access_key, 'query': query}
+
+        r = requests.get(url, params=payload)
+        j = r.json()
+
+        if j.get("request"):
+            return True, None
+        else:
+            return False, j.get("error").get("info")
+
+        
+    def streams(self, config: Mapping[str, Any]) -> List[Stream]:
+        return [
+            Forecast(config = config)
+        ]
